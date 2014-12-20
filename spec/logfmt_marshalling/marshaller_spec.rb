4e2c1b662ea1f0439e6be6a9e36b2824ca19c4ba
pm <mitin.pavel@gmail.com>
2014-12-20 12:52:51 +0200

    Add specs for LogfmtMarshalling::Marshaller.

--- spec/logfmt_marshalling/marshaller_spec.rb
+++ spec/logfmt_marshalling/marshaller_spec.rb
@@ -6,6 +6,67 @@ describe ::LogfmtMarshalling::Marshaller do
     expect(actual).to eq('')
   end
 
+  it 'drops true value' do
+    actual = marshal 'key' => true
+    expect(actual).to eq('key')
+  end
+
+  it 'serializes multiple single keys' do
+    actual = marshal 'key1' => true, 'key2' => true
+    expect(actual).to eq('key1 key2')
+  end
+
+  it 'serializes an unquoted value' do
+    actual = marshal 'key' => 'value'
+    expect(actual).to eq('key=value')
+  end
+
+  it 'preserves order in serialized pairs' do
+    actual = marshal 'key1' => 'value1', 'key2' => 'value2'
+    expect(actual).to eq('key1=value1 key2=value2')
+  end
+
+  it 'serializes mixed single/non-single pairs' do
+    actual = marshal 'key1' => 'value1', 'key2' => true
+    expect(actual).to eq('key1=value1 key2')
+  end
+
+  it 'serializes mixed pairs whatever the order' do
+    actual = marshal 'key1' => true, 'key2' => 'value2'
+    expect(actual).to eq('key1 key2=value2')
+  end
+
+  it 'quotes values with whitespaces' do
+    actual = marshal 'key' => 'a value'
+    expect(actual).to eq('key="a value"')
+  end
+
+  it 'serializes escaped quote value ' do
+    actual = marshal 'key' => 'quoted \" value', 'r' => 'esc\\t'
+    expect(actual).to eq('key="quoted \" value" r="esc\t"')
+  end
+
+  it 'serializes mixed pairs' do
+    actual = marshal 'key1' => 'quoted \" value', 'key2' => true, 'key3' => 'value3'
+    expect(actual).to eq('key1="quoted \" value" key2 key3=value3')
+  end
+
+  it 'serializes mixed characters pairs' do
+    actual = marshal 'foo' => 'bar',
+                     'a' => 14,
+                     'baz' => 'hello kitty',
+                     'ƒ' => '2h3s',
+                     'cool%story' => 'bro',
+                     'f' => true,
+                     '%^asdf' => true
+    expect(actual).to eq('foo=bar a=14 baz="hello kitty" ƒ=2h3s cool%story=bro f %^asdf')
+  end
+
+  it 'serializes an empty string' do
+    actual = marshal 'key' => ''
+    expect(actual).to eq('key=""')
+  end
+
   def marshal(data)
     described_class.new.marshal data
   end
